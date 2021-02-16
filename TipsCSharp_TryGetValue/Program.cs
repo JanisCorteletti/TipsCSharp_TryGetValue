66430e773bb58af7210d723d00a1871cade17771
Janis Leite Corteletti <janis.corteletti@gmail.com>
2021-02-16 21:21:41 +0100

    Add project files.

---
+++ TipsCSharp_TryGetValue/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TipsCSharp_TryGetValue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var example = new Examples();
+            example.ShowExample();
+        }
+    }
+}
